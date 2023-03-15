3d2c4b011c968f84cf732a587a58440493f32605
Anton Yaskov <yaskovanton333@gmail.com>
2023-03-15 15:37:02 +0500

    Add service and implementation

---
+++ FootBallers/Services/FootballersService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using FootBallers.Data;
+using FootBallers.Entities;
+using FootBallers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootBallers.Services
+{
+    public class FootballersService : IFootballersService
+    {
+        private readonly DataContext dataContext;
+        private readonly IMapper mapper;
+
+        public FootballersService(DataContext dataContext, IMapper mapper)
+        {
+            this.dataContext = dataContext;
+            this.mapper = mapper;
+        }
+
+        public async Task AddAsync(FootballerDto footballerDto)
+        {
+            var footballer = mapper.Map<Footballer>(footballerDto);
+            footballer.Id = new Guid();
+            await dataContext.AddAsync(footballer);
+            await dataContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Footballer>> GetAllAsync()
+        {
+            return await dataContext.Footballers.ToListAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var footballer = await dataContext.Footballers.FirstOrDefaultAsync(e => e.Id == id);
+            if (footballer != null)
+            {
+                dataContext.Footballers.Remove(footballer);
+                await dataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ FootBallers/Services/IFootballersService.cs
@@ -0,0 +1,12 @@
+﻿using FootBallers.Entities;
+using FootBallers.Models;
+
+namespace FootBallers.Services
+{
+    public interface IFootballersService
+    {
+        public Task AddAsync(FootballerDto footballer);
+        public Task<List<Footballer>> GetAllAsync();
+        public Task DeleteAsync(Guid id);
+    }
+}
