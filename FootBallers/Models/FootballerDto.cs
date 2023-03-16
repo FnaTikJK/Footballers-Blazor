ee72f729d271319f4ee0f633049c0c6e4145d56e
Anton Yaskov <yaskovanton333@gmail.com>
2023-03-16 13:26:47 +0500

    Add Simple validation

--- FootBallers/Models/FootballerDto.cs
+++ FootBallers/Models/FootballerDto.cs
@@ -1,4 +1,6 @@
-﻿namespace FootBallers.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootBallers.Models
 {
     public class FootballerDto
     {
@@ -10,5 +12,20 @@
         public string Team { get; set; }
         public string Country { get; set; }
         public bool IsChangeable { get; set; }
+
+        public static bool isValid(FootballerDto dto, out string error)
+        {
+            foreach (var propInfo in typeof(FootballerDto).GetProperties())
+            {
+                if (propInfo.GetValue(dto) == null || propInfo.GetValue(dto).ToString().Length == 0)
+                {
+                    error = $"Поле {propInfo.Name} не может быть пустым";
+                    return false;
+                }
+            }
+
+            error = "";
+            return true;
+        }
     }
 }
